ff54c3dd24c7459c3472566cfa0ebd5ede0c4192
ototadana <ototadana@gmail.com>
2022-05-29 12:33:27 +0900

    add state receiving

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ import time
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("", 8889))
 
+sock.sendto("command".encode(), ("192.168.10.1", 8889))
+sock.recvfrom(1024)
+
+state_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+state_sock.bind(("", 8890))
+data, _ = state_sock.recvfrom(1024)
+print()
+print(data.decode())
+
 print("Tello Python3 Demo.")
-print("Tello: command takeoff land flip forward back left right")
+print("Tello: takeoff land flip forward back left right")
 print("       up down cw ccw speed speed?")
 print("end -- quit demo.")
+
 
 while True:
     try:
