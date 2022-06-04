2283da765c49b8d22329fe570dea8215ef3731b9
ototadana <ototadana@gmail.com>
2022-06-04 12:45:22 +0900

    add state display function

--- gui.py
+++ gui.py
@@ -1,3 +1,7 @@
+import random
+import threading
+import time
+
 import PySimpleGUI as sg
 
 layout = [
@@ -16,9 +20,42 @@ layout = [
 ]
 window = sg.Window("My Drone", layout)
 
+
+class Info:
+    def __init__(self):
+        self.__state = {}
+
+    def set_states(self, states):
+        self.__state = states
+
+    def get_states(self):
+        return self.__state
+
+    def get_state(self, name):
+        return self.__state.get(name, 0.0)
+
+
+info = Info()
+
+
+def receive_state():
+    while True:
+        time.sleep(1)
+        try:
+            info.set_states({"bat": random.random() * 100})
+        except Exception:
+            print("\nExit . . .\n")
+            break
+
+
+state_receive_thread = threading.Thread(target=receive_state)
+state_receive_thread.start()
+
 while True:
     msg = ""
     event, values = window.read(timeout=1)
+    window["state"].update(f'battery: {info.get_state("bat"):.1f}%')
+
     if event == sg.WINDOW_CLOSED or event == "Quit":
         break
     if event == "OK":
