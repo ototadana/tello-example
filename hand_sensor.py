9b0264fbd28cb56fdd25436e9f2638a317fe2b81
ototadana <ototadana@gmail.com>
2022-07-24 10:46:25 +0900

    add forward and back commands

--- hand_sensor.py
+++ hand_sensor.py
@@ -32,20 +32,50 @@ class HandSensor(Startable):
                     continue
 
                 results = hands.process(image)
-                command = self.__get_command(results.multi_hand_landmarks)
+                command = self.__get_command(
+                    results.multi_hand_landmarks, results.multi_handedness
+                )
                 info.entry_command(command)
 
         logger.info("done")
 
-    def __get_command(self, multi_hand_landmarks: list) -> str:
+    def __get_command(self, multi_hand_landmarks: list, multi_handedness: list) -> str:
         if multi_hand_landmarks:
-            for hand_landmarks in multi_hand_landmarks:
-                command = self.__get_command_by_hand(hand_landmarks.landmark)
+            for i, hand_landmarks in enumerate(multi_hand_landmarks, 0):
+                left_hand = multi_handedness[i].classification[0].label == "Left"
+                command = self.__get_command_by_hand(hand_landmarks.landmark, left_hand)
                 if command != "":
                     return command
         return ""
 
-    def __get_command_by_hand(self, hlm: Sequence[Landmark]) -> str:
+    def __get_command_by_hand(self, hlm: Sequence[Landmark], left_hand: bool) -> str:
+        if self.__is_four_up(hlm):
+            return self.__get_forward_or_back(hlm, left_hand)
+
+        return self.__get_direction(hlm)
+
+    def __is_four_up(self, hlm: Sequence[Landmark]) -> bool:
+        if not (hlm[8].y < hlm[7].y < hlm[6].y < hlm[5].y < hlm[0].y):
+            return False
+        if not (hlm[12].y < hlm[11].y < hlm[10].y < hlm[0].y):
+            return False
+        if not (hlm[16].y < hlm[15].y < hlm[14].y < hlm[0].y):
+            return False
+        if not (hlm[20].y < hlm[19].y < hlm[18].y < hlm[0].y):
+            return False
+        return True
+
+    def __get_forward_or_back(self, hlm: Sequence[Landmark], left_hand: bool) -> str:
+        distance = hlm[8].x - hlm[20].x
+        if distance < 0.05 and distance > -0.05:
+            return ""
+
+        if distance < 0:
+            return "forward 20" if left_hand else "back 20"
+        else:
+            return "back 20" if left_hand else "forward 20"
+
+    def __get_direction(self, hlm: Sequence[Landmark]) -> str:
         a = self.__angle(hlm[5], hlm[6], hlm[8])
 
         if self.__is_up(hlm) and a > 60 and a < 120:
